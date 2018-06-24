f2b16009d87ad1f4523e72556ffd5aedb19789bf
shindig7 <jal457@drexel.edu>
2018-06-23 20:03:27 -0400

    Began work on TDEE calculator

--- metabolism.py
+++ metabolism.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+def TDEE(bmr): 
+    activity_dict = {'Sedentary': {'Description': 'Sedentary Lifestyle, Little or No Exercise, Moderate Walking, Desk Job', 'Modifier': 1.2}, 'Slightly Active': {'Description': 'Slightly Active, Exercise or Light Sports 1 to 3 Days a Week, Light Jogging or Walking 3 to 4 Days a Week', 'Modifier': 1.375}, 'Lightly Active': {'Description': 'Lightly Active, Exercise or Moderate Sports 2 to 3 Days a Week, Light Jogging or Walking 5 to 7 Days a Week', 'Modifier': 1.425}, 'Moderately Active': {'Description': 'Moderately Active, Physical Work, Exercise, or Sports 4 to 5 Days a Week, Construction Laborer', 'Modifier': 1.55},  'Very Active': {'Description': 'Very Active, Heavy Physical Work, Exercise, or Sports 6 to 7 Days a Week, Hard Laborer', 'Modifier': 1.75}, 'Extremely Active': {'Description': 'Extremely Active, Very Heavy Physical Work or Exercise Every Day, Professional/Olympic Athlete', 'Modifier': 1.9}                      }
+    print(activity_dict['Lightly Active']['Modifier'])
+
+
 def BMR(age, height, weight, gender, units='imperial'):
     if units == 'imperial':
         height = height * 2.54
@@ -15,7 +20,11 @@ def BMR(age, height, weight, gender, units='imperial'):
 
     return bmr
 
+def main():
+    #bmr = BMR(21, 74, 272, 'M')
+    #print(bmr)
+    TDEE(1)
+
 
 if __name__ == "__main__":
-    bmr = BMR(21, 74, 272, 'M')
-    print(bmr)
+    main()
