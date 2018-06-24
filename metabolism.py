1a4831391e23b6f6106d028e026a8ae249f423bf
shindig7 <jal457@drexel.edu>
2018-06-23 20:52:08 -0400

    Started adding documentation, finished function

--- metabolism.py
+++ metabolism.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 
 def TDEE(bmr): 
-    activity_dict = {'Sedentary': {'Description': 'Sedentary Lifestyle, Little or No Exercise, Moderate Walking, Desk Job', 'Modifier': 1.2}, 'Slightly Active': {'Description': 'Slightly Active, Exercise or Light Sports 1 to 3 Days a Week, Light Jogging or Walking 3 to 4 Days a Week', 'Modifier': 1.375}, 'Lightly Active': {'Description': 'Lightly Active, Exercise or Moderate Sports 2 to 3 Days a Week, Light Jogging or Walking 5 to 7 Days a Week', 'Modifier': 1.425}, 'Moderately Active': {'Description': 'Moderately Active, Physical Work, Exercise, or Sports 4 to 5 Days a Week, Construction Laborer', 'Modifier': 1.55},  'Very Active': {'Description': 'Very Active, Heavy Physical Work, Exercise, or Sports 6 to 7 Days a Week, Hard Laborer', 'Modifier': 1.75}, 'Extremely Active': {'Description': 'Extremely Active, Very Heavy Physical Work or Exercise Every Day, Professional/Olympic Athlete', 'Modifier': 1.9}                      }
-    print(activity_dict['Lightly Active']['Modifier'])
+    """Calculates an estimated Total Daily Energy Expenditure (TDEE) based on
+    inputted activity level and an estimated BMR
+    
+    Input:
+        bmr (int)
+            Basal Metabolic Rate, calculated from the BMR function or an
+            outside function """
+    activity_dict = {'Sedentary': {'Description': 'Little or No Exercise, Moderate Walking, Desk Job', 'Modifier': 1.2}, 'Slightly Active': {'Description': 'Exercise or Light Sports 1 to 3 Days a Week, Light Jogging or Walking 3 to 4 Days a Week', 'Modifier': 1.375}, 'Lightly Active': {'Description': 'Exercise or Moderate Sports 2 to 3 Days a Week, Light Jogging or Walking 5 to 7 Days a Week', 'Modifier': 1.425}, 'Moderately Active': {'Description': 'Physical Work, Exercise, or Sports 4 to 5 Days a Week, Construction Laborer', 'Modifier': 1.55},  'Very Active': {'Description': 'Heavy Physical Work, Exercise, or Sports 6 to 7 Days a Week, Hard Laborer', 'Modifier': 1.75}, 'Extremely Active': {'Description': 'Very Heavy Physical Work or Exercise Every Day, Professional/Olympic Athlete', 'Modifier': 1.9}                      }
+
+    for i, L in enumerate(sorted(activity_dict, key=lambda k:
+        activity_dict[k]['Modifier'])):
+        print("{}. {}: {}".format(str(i+1), L, activity_dict[L]['Description']))
+
+    level = int(input('Please select an activity level: ')) - 1
+    selected_level = activity_dict[sorted(activity_dict, key=lambda k:
+        activity_dict[k]['Modifier'])[level]]
+
+    activity_modifier = selected_level['Modifier']
+
+    tdee = bmr * activity_modifier
+
+    print('BMR: {}\nTDEE: {}'.format(bmr, tdee))
 
 
 def BMR(age, height, weight, gender, units='imperial'):
@@ -23,7 +43,7 @@ def BMR(age, height, weight, gender, units='imperial'):
 def main():
     #bmr = BMR(21, 74, 272, 'M')
     #print(bmr)
-    TDEE(1)
+    TDEE(2145)
 
 
 if __name__ == "__main__":
